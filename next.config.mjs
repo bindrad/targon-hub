b851a52e6f01adec91d7cf1422cbf7947a1fdd87
bindrad <dhruv@manifold.inc>
2025-01-20 13:08:30 -0700

    error code
    Signed-off-by: bindrad <dhruv@manifold.inc>

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   images: {
-    remotePatterns: [
+      remotePatterns: [
       {
         protocol: "https",
         hostname: "serpapi.com",
